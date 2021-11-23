8145bceb9ddf65763b463c9a515a047ca3ae7d44
Anthony Rawlings <rawlings808@hotmail.com>
2021-11-22 21:07:49 -0600

    complete

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // TODO: add code here
+
+window.addEventListener("load", function () {
+    fetch(
+        "https://handlers.education.launchcode.org/static/astronauts.json"
+    ).then(function (response) {
+        response.json().then(function (json) {
+            console.log(json);
+            const container = document.getElementById("container");
+            let innerHTML = "";
+            
+            for (a of json) {
+                innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                    <h3>${a.firstName} ${a.lastName}</h3>
+                    <ul>
+                        <li>Hours in Space: ${a.hoursInSpace}</li>
+                        <li>Active: ${a.active} </li>
+                        <li>Skills: ${a.skills.join(', ')} </li>
+                    </ul>
+                </div>
+            <img class="avatar" src="${a.picture}">
+        </div>`
+            };
+            container.innerHTML = innerHTML;
+
+        });
+    });
+});
